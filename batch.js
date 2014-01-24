66c0230b886548d7e832ccf94773bc25b3630693
etissieres <etienne.tissieres@gmail.com>
2014-01-24 11:34:31 +0100

    batch : message replay & some bug fixes

--- batch.js
+++ batch.js
@@ -1,12 +1,14 @@
 var hubiquitus = require('hubiquitus-core');
+var hubiquitusQos = require('./index');
 var logger = hubiquitus.logger('hubiquitus:addons:qos:batch');
+hubiquitus.logger.enable('hubiquitus:addons:qos:batch', 'trace');
 var commander = require('commander');
 var mongo = require('mongodb');
 
 commander
   .version('0.0.1')
   .option('-d, --debug', 'Debug')
-  .option('-disc', '--discovery [str]', 'Discovery addr')
+  .option('-disc, --discovery [str]', 'Discovery addr')
   .option('-gc, --gc-interval [n]', 'GC interval', parseInt)
   .option('-t, --timeout [n]', 'Processing timeout', parseInt)
   .option('-mhost, --mongo-host [str]', 'Mongo host')
@@ -27,6 +29,7 @@ var conf = {
     collection: commander.mongoCollection || 'queue'
   }
 };
+logger.info('using configuration', {conf: conf});
 
 hubiquitus.start({discoveryAddr: conf.discovery});
 
@@ -49,36 +52,59 @@ function main() {
     logger.trace('garbage collector iteration begins');
     var query = {
       $or: [
-        {date: {$gte: Date.now() + conf.timeout}},
+        {date: {$lt: (Date.now() - conf.timeout)}},
         {error: true}
       ]
     };
 
-    var cursor = db.collection.find(query);
-    var count = cursor.count();
-    logger.trace('garbage collector iteration processing ' + count + ' items', {query: query});
-
-    var stream = cursor.stream();
-
-    stream.on('data', function (item) {
-      if (item.error) {
-        // replay message
-      } else {
-        hubiquitus.send('qosBatch', item.req.to, null, function (err) {
-          if (err && err.code === 'TIMEOUT') {
-            // replay message
-          }
-        }, {ping: true});
+    var cursor = db.collection(conf.mongo.collection).find(query);
+    cursor.count(function (err, count) {
+      if (err) {
+        return logger.error('garbage collector iteration error', {query: query, err: err});
       }
-    });
 
-    stream.on('close', function () {
-      logger.trace('garbage collector iteration ends');
-    });
+      logger.trace('garbage collector iteration processing ' + count + ' items', {query: query});
 
-    stream.on('error', function (err) {
-      logger.error('garbage collector iteration error', {query: query, err: err});
-    });
+      var stream = cursor.stream();
 
+      stream.on('data', function (item) {
+        if (item.error) {
+          logger.trace('item in error; request replay', {item: item});
+          replay(item._id, item.req);
+        } else {
+          logger.trace('timeout reached; try to ping handler...', {item: item});
+          hubiquitus.send('qosBatch', item.req.to, null, function (err) {
+            if (err && err.code === 'TIMEOUT') {
+              logger.trace('handler dead; request replay', {item: item});
+              replay(item._id, item.req);
+            } else if (!err) {
+              logger.trace('handler alive; let\'s give him more time', {item: item});
+            } else {
+              logger.warn('handler ping returns error', {item: item, err: err});
+            }
+          }, {ping: true});
+        }
+      });
+
+      stream.on('close', function () {
+        logger.trace('garbage collector iteration ends');
+      });
+
+      stream.on('error', function (err) {
+        logger.error('garbage collector iteration error', {query: query, err: err});
+      });
+    });
   }, conf.gcInterval);
+}
+
+function replay(safeId, req) {
+  hubiquitusQos.send(req.from, hubiquitus.utils.aid.bare(req.to), req.content, function (err) {
+    if (!err) {
+      db.collection(conf.mongo.collection).remove({_id: safeId}, function (err) {
+        if (err) {
+          logger.error('failed to remove replayed req from queue, may be processed twice !', {_id: safeId, err: err});
+        }
+      });
+    }
+  });
 }
